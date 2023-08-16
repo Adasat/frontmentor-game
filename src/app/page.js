d03e45345310b3cb3e7ace33557098abaacfd855
Adasat <pbonbol@gmail.com>
2023-08-16 17:19:34 +0100

    added paper icon!

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,13 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import bgTriangle from '../../public/bg-triangle.svg'
+import paperIcon from '../../public/icon-paper.svg'
 
 export default function Home() {
   const [score, setScore] = useState(0)
   return (
     <>
-      <div className="flex flex-col justify-center items-center md:w-3/5 p-20">
+      <div className="flex flex-col justify-center items-center sm:w-full md:w-3/5 p-20">
         <div className="flex flex-row justify-between w-4/5 p-4 rounded-lg border-2 border-headerOutline">
           <div className="flex flex-col font-barlow font-bold text-3xl ">
             <span>ROCK</span>
@@ -24,18 +25,15 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="w-4/5 h-4/5">
           <div
-            className="flex flex-wrap justify-center items-center bg-contain bg-center bg-no-repeat mt-36"
+            className="flex flex-wrap w-4/5 h-4/5 justify-center  bg-contain bg-center bg-no-repeat mt-36"
             style={{
               backgroundImage: `url(${bgTriangle.src})`,
               height: "200px",
             }}
           >
-            <div className="w-6/12 bg-yellow-100">
-              <button className="bg-scissors1 p-4 rounded-full">
-                {/* Contenido del botón */}
-              </button>
+            <div className="flex justify-center items-center w-6/12 rounded-xl bg-paper1">
+              <Image src={paperIcon} width={100} height={100} alt='Paper icon' />
             </div>
             <div className="w-6/12 bg-blue-100">
               <button className="bg-paper1 p-4 rounded-full">
@@ -51,7 +49,6 @@ export default function Home() {
         </div>
 
         <div className="flex items-end">Rules</div>
-      </div>
 
       <div className="text-md text-center ">
         Challenge by{" "}
